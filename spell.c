887ca466446c2e98324b43494127bacceffed774
Heather Moore <moore.hj@gmail.com>
2019-09-24 17:49:37 +0200

    refactor: all passing except buffer overflow

--- spell.c
+++ spell.c
@@ -11,15 +11,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <errno.h>
 #include "dictionary.h"
 
-void lower_string(char s[]) {
+void lower_string(char word_with_punct[]) {
    int c = 0;
    
-   while (s[c] != '\0') {
-      if (s[c] >= 'A' && s[c] <= 'Z') {
-         s[c] = s[c] + 32;
+   // Convert each letter to lowercase via ascii code
+   while (word_with_punct[c] != '\0') {
+      if (word_with_punct[c] >= 'A' && word_with_punct[c] <= 'Z') {
+         word_with_punct[c] = word_with_punct[c] + 32;
       }
       c++;
    }
@@ -32,7 +32,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
       hashtable[i] = NULL;
     }
 
-    // Open dictionary file
+    // Open dictionary file, returning false if not found
     FILE* file = fopen(dictionary_file, "r");
     if (file == NULL)
     {
@@ -41,53 +41,52 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
     }
 
     // Initiate variable to store current word
-    char* dword = calloc(LENGTH+ 1,sizeof(char)); 
+    char* lowered_word = calloc(LENGTH+ 1,sizeof(char)); 
     
     // For each line, get the word
     char* line = NULL;  
     size_t len = 45;
     ssize_t read;
 
-    line = (char *)malloc(len * sizeof(char));
-    if( line == NULL)
-    {
-        perror("Not able to allocate buffer for line.");
-        exit(1);
-    }
-
     while ((read = getline(&line, &len, file)) != -1) {
+        // Remove newline captured by getline function
         if ((line)[read - 1] == '\n') 
         {
           (line)[read - 1] = '\0';
           --read;
         } 
-        dword = line;
-        lower_string(dword);
-        node* new_node = malloc(sizeof(node));
-        strcpy(new_node->word,dword);
 
-        // Add new lowered word to hashtable
-        int hashkey = hash_function(dword);
+        // Lowercase the word
+        lowered_word = line;
+        lower_string(lowered_word);
+
+        // Add lowered word to hashtable
+        node* new_node = malloc(sizeof(node));
+        strcpy(new_node->word,lowered_word);
+        int hashkey = hash_function(lowered_word);
         new_node->next = hashtable[hashkey];
         hashtable[hashkey] = new_node; 
     }
 
-    // Handle any unexpected errors
+    // Handle any unexpected errors with the file
     if (ferror(file)) {
         puts("Experienced an error with the dictionary file.");
     }
 
-    // Free the line and close the file
-    free(line);
+    // Free the lowered_word, reset line and close the file
+    free(lowered_word);
     line = NULL;
     fclose(file);
     return true;    
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]) {
+  // Lowercase the word
   char* lower_word = calloc(LENGTH+ 1,sizeof(char)); 
   strcpy(lower_word, word);
   lower_string(lower_word);
+
+  // Iterate through the hash to look for the related bucket
   int bucket = hash_function(lower_word);
   hashmap_t cursor = hashtable[bucket];
   while (cursor != NULL) {
@@ -104,21 +103,11 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
 
   // For each line, get the word
   char* line = NULL;  
-  size_t len = 1000;
+  size_t len = 0;
   ssize_t read;
 
-  line = (char *)malloc(len * sizeof(char));
-  if( line == NULL)
-  {
-      perror("Not able to allocate buffer for line.");
-      exit(1);
-  }
-
-  read = getline(&line, &len, fp);
-  if (errno == ENOMEM || errno == EOVERFLOW) {
-    num_misspelled++;
-  }
-  while (read >= 0) {
+  while ((read = getline(&line, &len, fp)) != -1) {
+      // Remove newline captured by getline function
       if ((line)[read - 1] == '\n') 
       {
         (line)[read - 1] = '\0';
@@ -127,29 +116,31 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
 
       // Tokenize string on spaces
       char* token;
-      token = strtok(line, " ");
-      if (token == NULL)
-          return -1;
+      char* delimiter = " ";
+      token = strtok(line, delimiter);
+      if (token == NULL){
+        return -1;
+      }
+
+      // While there are tokenized words    
       do
       {
         // Strip start and end punctuation
         int i;
-        char* p;
-        for(i=strlen(token)-1; ispunct(token[i]);--i)
-            token[i]='\0';
-  
-        for(p=token;ispunct(*p);++p);
+        char* stripped_word;
+        for(i=strlen(token)-1; ispunct(token[i]);--i) {
+          token[i]='\0';
+        }
+        for(stripped_word=token;ispunct(*stripped_word);++stripped_word);
 
-        // Validate remaining word/token
-        bool result = check_word(p, hashtable); 
-        if (!result){
-          misspelled[num_misspelled] = p;
+        // Validate stripped word
+        bool word_found = check_word(stripped_word, hashtable); 
+        if (!word_found){
+          misspelled[num_misspelled] = stripped_word;
           num_misspelled++;
         }
         line = NULL;
-      } while ((token = strtok(NULL, " ")) != NULL);
-
-      read = getline(&line, &len, fp);
+      } while ((token = strtok(NULL, delimiter)) != NULL);
   }
 
   // Handle any unexpected errors
